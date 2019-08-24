0e104b2bb772a3dea366eb43a65f4afffc4e323d
Joseph Kinahan <kinahanj25@gmail.com>
2019-08-24 23:57:06 +0100

    v1

---
+++ src/app/app-material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import * as Material from '@angular/material';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    Material.MatToolbarModule
+  ],
+  exports: [
+    Material.MatToolbarModule
+  ]
+})
+export class AppMaterialModule { }
